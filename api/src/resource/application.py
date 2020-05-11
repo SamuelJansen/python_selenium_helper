1e2f9c466c39275ecedcec78c8bdd0b6535a035d
Samuel Jansen <samuel.jansen@cwi.com.br>
2020-05-10 22:20:34 -0300

    resource/update

--- api/src/resource/application.py
+++
@@ -1,8 +0,0 @@
-application:
-  fps: 30
-  aps: 30
-  screen:
-    size: [FULL_SCREEN,FULL_SCREEN] ###- [960x680] [1200x890] [430x200] [1200x780] [1920x1080]
-    position: [0,0]
-  color: {black: (0,0,0), white: (255,255,255), backgroundColor: (237,201,202), red: (255,0,0), niceBlue: (0,0,150)}
-  scale-range: 1000
